b002261c984ea433a94d349c58b2ec38234e1af8
Steven <scroft001@gmail.com>
2019-02-14 18:33:35 -0800

    Player Movement

--- Color-Run-Testing/Assets/Scripts/PlayerMovement.cs
+++ Color-Run-Testing/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	//variables
+	private Vector3 mousePosition;
+	public Rigidbody2D rb;
+	private Vector2 direction;
+	public float moveSpeed = 100f;
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if the mouse is held down (touching the screen)
+		if(Input.GetMouseButton(0)){
+			//find the mouses position
+			mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			//set the direction to be that of the mouse
+			direction = (mousePosition - transform.position).normalized;
+			//move the dot
+			rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+		}
+		else{
+			rb.velocity = Vector2.zero;
+		}
 	}
 }
