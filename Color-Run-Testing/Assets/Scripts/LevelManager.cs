b10db11b8c3d090d0ddbc468248356885e1b4ce2
Steven <scroft001@gmail.com>
2019-02-19 17:00:59 -0800

    Level one done

---
+++ Color-Run-Testing/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D info){
+		if(info.gameObject.tag == "Player"){
+			Debug.Log ("Finished");
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
